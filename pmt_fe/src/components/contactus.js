e3dee6ecae3b2ceafa21d15c3cc51ad99f65033d
yashaswi maharshi <maha.yash@gmail.com>
2018-11-18 21:12:41 +0530

    contact us done

--- pmt_fe/src/components/contactus.js
+++ pmt_fe/src/components/contactus.js
@@ -1,10 +1,155 @@
 import React from "react";
+import bgImage2 from "../images/main-slider/image-6-1.jpg";
 
 class ContactUs extends React.Component {
   render() {
     return (
       <div>
-        <h1>Contact us</h1>
+        {/* <!--Page Title--> */}
+        <section
+          className="page-title"
+          style={{ backgroundImage: `url(${bgImage2})` }}
+        >
+          <div className="auto-container">
+            <h1>Contact Us</h1>
+            <div className="text">contact us for more information</div>
+          </div>
+          {/* <!--Page Info--> */}
+          <div className="page-info">
+            <div className="auto-container clearfix">
+              <div className="pull-left">
+                <ul className="social-icon-one">
+                  <li className="share">Share on :</li>
+                  <li>
+                    <a className="fa fa-facebook-f" href="#" />
+                  </li>
+                  <li>
+                    <a className="fa fa-twitter" href="#" />
+                  </li>
+                  <li>
+                    <a className="fa fa-google-plus" href="#" />
+                  </li>
+                </ul>
+              </div>
+              <div className="pull-right">
+                <ul className="bread-crumb clearfix">
+                  <li>
+                    <a href="index.html">Home</a>
+                  </li>
+                  <li>Contact</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/* <!--End Page Title--> */}
+
+        {/* <!--Services Section--> */}
+        <section className="contact-section">
+          <div className="auto-container">
+            <div className="row clearfix">
+              {/* <!--Column--> */}
+              <div className="column col-md-8 col-sm-12 col-xs-12">
+                <h2>Get in Touch</h2>
+                <div className="text">
+                  if you want to get more info, ping us now.
+                </div>
+                {/* <!-- Contact Form --> */}
+                <div className="contact-form">
+                  {/* <!--Contact Form--> */}
+                  <form method="post" action="sendemail.php" id="contact-form">
+                    <div className="row clearfix">
+                      <div className="col-md-6 col-sm-6 col-xs-12 form-group">
+                        <input
+                          type="text"
+                          name="username"
+                          placeholder="Name"
+                          required
+                        />
+                      </div>
+
+                      <div className="col-md-6 col-sm-6 col-xs-12 form-group">
+                        <input
+                          type="email"
+                          name="email"
+                          placeholder="Email"
+                          required
+                        />
+                      </div>
+
+                      <div className="col-md-12 col-sm-12 col-xs-12 form-group">
+                        <input
+                          type="text"
+                          name="subject"
+                          placeholder="Subject"
+                          required
+                        />
+                      </div>
+
+                      <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 form-group">
+                        <textarea name="message" placeholder="Massage" />
+                      </div>
+
+                      <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 form-group text-right">
+                        <button
+                          className="theme-btn btn-style-one"
+                          type="submit"
+                          name="submit-form"
+                        >
+                          Send Now
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+                {/* <!--End Contact Form --> */}
+              </div>
+              {/* <!--Column--> */}
+              <div className="column col-md-4 col-sm-12 col-xs-12">
+                <h2>Contact info</h2>
+                <div className="text">
+                  if you want to get more info, ping us now.
+                </div>
+                <ul className="list-style-two">
+                  <li>
+                    <span className="icon fa fa-volume-control-phone" />
+                    <strong>+91 97852 02444</strong>Mon-Fri, 9am until 6pm
+                  </li>
+                  <li>
+                    <span className="icon fa fa-envelope-o" />
+                    <strong style={{ fontSize: "16px" }}>
+                      support@pratapengineeringtechnology.com
+                    </strong>
+                    We reply within 24 hours
+                  </li>
+                  <li>
+                    <span className="icon fa fa-map-marker" />
+                    A/204,Virat Prime, plot no-24-25,Shiv Nagar A Niwaru road
+                    Jhotwara Jaipur-302012, Rajasthan, IN
+                  </li>
+                </ul>
+                <ul className="social-icon-two">
+                  <li>
+                    <a href="#">
+                      <span className="fa fa-facebook" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <span className="fa fa-twitter" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <span className="fa fa-google-plus" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/* <!--End Services Section--> */}
       </div>
     );
   }
