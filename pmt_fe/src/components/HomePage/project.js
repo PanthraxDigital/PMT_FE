2aad60fbe5bfd1744855877815aebed490b413b9
yashaswi maharshi <maha.yash@gmail.com>
2019-04-25 18:20:18 +0530

    added new pics to the home page

--- pmt_fe/src/components/HomePage/project.js
+++ pmt_fe/src/components/HomePage/project.js
@@ -67,6 +67,126 @@ class Project extends React.Component {
               <div class="p-tabs-content">
                 {/* <!-- Gallery Item --> */}
                 <Slider ref={c => (this.slider = c)} {...settings}>
+                <div class="gallery-item">
+                    <div class="inner-box">
+                      <figure class="image-box">
+                        <img
+                          src="https://live.staticflickr.com/65535/47642716562_78a39f8bcb_n.jpg"
+                          alt=""
+                        />
+                        <div style={{display : "none"}}>
+                          <h3 style={projectCaption}>
+                            Press tool for casting trimming
+                          </h3>
+                        </div>
+                      </figure>
+                    </div>
+                  </div>
+                  <div class="gallery-item">
+                    <div class="inner-box">
+                      <figure class="image-box">
+                        <img
+                          src="https://live.staticflickr.com/65535/33818371618_c1594d2efc_n.jpg"
+                          alt=""
+                        />
+                        <div style={{display : "none"}}>
+                          <h3 style={projectCaption}>
+                            Press tool for casting trimming
+                          </h3>
+                        </div>
+                      </figure>
+                    </div>
+                  </div>
+                  <div class="gallery-item">
+                    <div class="inner-box">
+                      <figure class="image-box">
+                        <img
+                          src="https://live.staticflickr.com/65535/40729441083_4135b96cc2_n.jpg"
+                          alt=""
+                        />
+                        <div style={{display : "none"}}>
+                          <h3 style={projectCaption}>
+                            Press tool for casting trimming
+                          </h3>
+                        </div>
+                      </figure>
+                    </div>
+                  </div>
+                  <div class="gallery-item">
+                    <div class="inner-box">
+                      <figure class="image-box">
+                        <img
+                          src="https://live.staticflickr.com/65535/47642716862_f9bd8d2bed_n.jpg"
+                          alt=""
+                        />
+                        <div style={{display : "none"}}>
+                          <h3 style={projectCaption}>
+                            Press tool for casting trimming
+                          </h3>
+                        </div>
+                      </figure>
+                    </div>
+                  </div>
+                  <div class="gallery-item">
+                    <div class="inner-box">
+                      <figure class="image-box">
+                        <img
+                          src="https://live.staticflickr.com/65535/33818371738_77e74dc3a5_n.jpg"
+                          alt=""
+                        />
+                        <div style={{display : "none"}}>
+                          <h3 style={projectCaption}>
+                            Press tool for casting trimming
+                          </h3>
+                        </div>
+                      </figure>
+                    </div>
+                  </div>
+                  <div class="gallery-item">
+                    <div class="inner-box">
+                      <figure class="image-box">
+                        <img
+                          src="https://live.staticflickr.com/65535/40729441243_56d1e0d812_n.jpg"
+                          alt=""
+                        />
+                        <div style={{display : "none"}}>
+                          <h3 style={projectCaption}>
+                            Press tool for casting trimming
+                          </h3>
+                        </div>
+                      </figure>
+                    </div>
+                  </div>
+                  <div class="gallery-item">
+                    <div class="inner-box">
+                      <figure class="image-box">
+                        <img
+                          src="https://live.staticflickr.com/65535/33818371878_65869d9415_n.jpg"
+                          alt=""
+                        />
+                        <div style={{display : "none"}}>
+                          <h3 style={projectCaption}>
+                            Press tool for casting trimming
+                          </h3>
+                        </div>
+                      </figure>
+                    </div>
+                  </div>
+                  <div class="gallery-item">
+                    <div class="inner-box">
+                      <figure class="image-box">
+                        <img
+                          src="https://live.staticflickr.com/65535/33818373828_77a92cfeef_n.jpg"
+                          alt=""
+                        />
+                        <div style={{display : "none"}}>
+                          <h3 style={projectCaption}>
+                            Press tool for casting trimming
+                          </h3>
+                        </div>
+                      </figure>
+                    </div>
+                  </div>
                   <div class="gallery-item">
                     <div class="inner-box">
                       <figure class="image-box">
@@ -89,7 +209,7 @@ class Project extends React.Component {
                             </div>
                           </div>
                         </div> */}
-                        <div>
+                        <div style={{display : "none"}}>
                           <h3 style={projectCaption}>Tube Bending Parts</h3>
                         </div>
                       </figure>
@@ -102,22 +222,7 @@ class Project extends React.Component {
                           src="https://farm5.staticflickr.com/4851/31441676597_43cd160334_n.jpg"
                           alt=""
                         />
-                        {/* <!-- Overlay Box --> */}
-                        {/* <div class="overlay-box">
-                          <div class="overlay-inner">
-                            <a href="projects-detail.html" class="image-link">
-                              <span class="icon fa fa-link" />
-                            </a>
-                            <div class="content">
-                              <h3>
-                                <a href="projects-detail.html">
-                                  Press tool for casting trimming
-                                </a>
-                              </h3>
-                            </div>
-                          </div>
-                        </div> */}
-                        <div>
+                        <div style={{display : "none"}}>
                           <h3 style={projectCaption}>
                             Press tool for casting trimming
                           </h3>
@@ -132,23 +237,7 @@ class Project extends React.Component {
                           src="https://farm5.staticflickr.com/4860/31441596257_c9e54db5e8_n.jpg"
                           alt=""
                         />
-                        {/* <!-- Overlay Box --> */}
-                        {/* <div class="overlay-box">
-                          <div class="overlay-inner">
-                            <a href="projects-detail.html" class="image-link">
-                              <span class="icon fa fa-link" />
-                            </a>
-                            <div class="content">
-                              <h3>
-                                <a href="projects-detail.html">
-                                  Match plate for sand casting
-                                </a>
-                              </h3>
-                              
-                            </div>
-                          </div>
-                        </div> */}
-                        <div>
+                        <div style={{display : "none"}}>
                           <h3 style={projectCaption}>
                             Match plates for Sand Casting
                           </h3>
